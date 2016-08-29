c267f541e1f591a325992c01c67d1291447cbf0d
Eric Chamberlain <eric.j.chamberlain@nordstrom.com>
2016-08-29 09:13:30 -0700

    Adding semicolons back. These macros are different in that they inject methods, which do not need an end semicolon. To make the entire lib consistent all semicolons have been added to macros.

--- objc/heroin/di-cedar.h
+++ objc/heroin/di-cedar.h
@@ -11,9 +11,9 @@
 #import "di.h"
 
 #define di_fake() \
-[DIServiceLocator setInstance:nice_fake_for([DIServiceLocator class])]
+[DIServiceLocator setInstance:nice_fake_for([DIServiceLocator class])];
 
 #define di_stub(PROPERTY_NAME) \
-[DIServiceLocator getInstance] stub_method(@selector(getDependency:)).with(@OS_STRINGIFY(PROPERTY_NAME)).and_return(PROPERTY_NAME)
+[DIServiceLocator getInstance] stub_method(@selector(getDependency:)).with(@OS_STRINGIFY(PROPERTY_NAME)).and_return(PROPERTY_NAME);
 
 #endif /* di_cedar_h */
