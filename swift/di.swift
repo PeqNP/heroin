efb10c94b519b3a70f67a00e2e0ed24f96d83e9a
Randall Brown and Robert McEntire <randall.brown+robert.mcentire@nordstrom.com>
2016-11-22 14:00:34 -0800

    Cleaned up a lot of the code. Can now provide the class instance you want to inject, instead of providing string. This provides type safety.

--- swift/di.swift
+++ swift/di.swift
@@ -1,27 +1,126 @@
-import Foundation
-import NBApi
+/**
+ Provides a simple method to inject dependencies.
+ 
+ - copyright: (c) 2016 Upstart Illustration LLC. All rights reserved.
+ */
 
-protocol DIContainer {
+import Foundation
+
+private let di = DIServiceLocator()
+
+/**
+ Register a container with the DI service.
+ 
+ - parameter containers: A list of DIContainer.
+ */
+public func di_register(containers: [DIContainer]) {
+    for container in containers {
+        di.register(container: container)
+    }
+}
+
+/**
+ Register a single container with the DI Service.
+
+ */
+public func di_register(container: DIContainer) {
+    di.register(container: container)
+}
+
+/**
+ Inject a dependency given the class type.
+
+ This is used when the name of the property is the same name as the class with the first letter is lowercase:
+ ```
+ public let metricsService: MetricsService
+ ```
+ 
+ When the name of the property deviates from the name of the class you would use the di_inject(name:) method.
+ 
+ - parameter property: The class of the dependency to inject.
+ - returns: The instance of the dependency. The app will crash if the dependency is not found.
+ */
+public func di_inject(_ property: AnyClass) -> Any {
+    let className = String(describing: property)
+    var chars = Array(className.characters)
+    let firstLetter = String(chars[0]).lowercased()
+    let firstLetterChars = Array(firstLetter.characters)
+    chars[0] = firstLetterChars[0]
+    let propertyName = String(chars)
+    return di.inject(propertyName)
+}
+
+/**
+ Inject a dependency given the class's property name.
+ 
+ When the name of the property does not follow the convention, where the property name of the dependency is the same name as the class but with a lowercase first letter, you will use this method.
+ 
+ This is necessary when you have two different instances of the same class. A common use is when defining an instance of an HTTP builder, where one is a builder for non-secure URLs using 'http' and another for secure URLS using 'https':
+ ```
+ public let httpService: HTTPService // Use di_inject(property: AnyClass) for this property
+ public let httpsService: HTTPService // Use di_inject(name: String) for this property
+ ```
+ 
+ Here is an example of how you would inject both of these dependencies:
+ ```
+ class MyClass {
+    private let httpService = di_inject(HTTPService.self)
+    private let httpsService = di_Inject("httpsService")
+ }
+ ```
+ 
+ - parameter name: The name of the property assocaited to the dependency.
+ - returns: The instance of the dependency. The app will crash if the dependency is not found.
+ */
+public func di_inject(_ name: String) -> Any {
+    return di.inject(name)
+}
+
+/**
+ Clear all dependencies previously used by the DI. This should only be used in test before.
+
+ */
+public func di_clear() {
+    di.clear()
+}
+
+/**
+ Associate a class dependency with a value. This should only be used in test.
+
+ */
+public func di_stub(class: AnyClass, value: Any) {
 
 }
 
-struct DIDependency {
+/**
+ Associate a name of a dependency with corresponding value. This should only be used in test.
+ 
+ */
+public func di_stub(name: AnyClass, value: Any) {
+
+}
+
+public protocol DIContainer {
+
+}
+
+private struct DIDependency {
     public let container: DIContainer?
     public let value: Any
 }
 
-class DIServiceLocator {
+private class DIServiceLocator {
     private var containers: [DIContainer] = [DIContainer]()
     private var dependencies: [String : DIDependency] = [String : DIDependency]()
 
     /**
      Inject a property with a dependency registered with the service locator.
      */
-    internal func inject(_ property: String) -> Any? {
+    internal func inject(_ property: String) -> Any {
         if let dependency = dependencies[property] {
             return dependency.value
         }
-        return nil
+        assert(false, "property \(property) has not been registered with any container.")
     }
 
     /**
@@ -42,7 +141,7 @@ class DIServiceLocator {
         containers.append(container)
     }
 
-    internal func clean() {
+    internal func clear() {
         containers = [DIContainer]()
         dependencies = [String : DIDependency]()
     }
